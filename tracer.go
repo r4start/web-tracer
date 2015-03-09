7947ae0e8a392fa5df34563faab80b394a047b69
r4start <r4start@gmail.com>
2015-03-09 23:12:01 +0300

    Added command-line options for app.

--- tracer.go
+++ tracer.go
@@ -1,46 +1,39 @@
 package main
 
 import (
+  _ "os"
   "fmt"
+  "flag"
   "net/http"
-  "os"
-  "database/sql"
-  "github.com/lib/pq"
 )
 
-var is_success bool = false
+func getServeAddress() (string, string) {
+  var host string
+  var port string
+
+  flag.StringVar(&host, "host", "localhost", "IP address for listening")
+  flag.StringVar(&port, "port", "4000", "Port number")
+
+  flag.Parse()
+
+  return host, port
+}
 
 func main() {
-  db_host := os.Getenv("OPENSHIFT_POSTGRESQL_DB_HOST")
-  db_port := os.Getenv("OPENSHIFT_POSTGRESQL_DB_PORT")
-  db_user := "admin4egsj8v"
-  db_pwd := "yAdiWMWAicBu"
-  db_name := "tracer"
-
-  db_conn_line := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s",
-                              db_host, db_port, db_user, db_pwd, db_name)
-
-  _, err := sql.Open("postgres", db_conn_line)
-  if err == nil {
-    is_success = true
-  }
+  host, port := getServeAddress()
 
   http.HandleFunc("/", hello)
-  bind := fmt.Sprintf("%s:%s", os.Getenv("HOST"), os.Getenv("PORT"))
+  bind := fmt.Sprintf("%s:%s", host, port)
+  
   fmt.Printf("listening on %s...", bind)
-  err = http.ListenAndServe(bind, nil)
+  
+  err := http.ListenAndServe(bind, nil)
+  
   if err != nil {
     panic(err)
   }
 }
 
 func hello(res http.ResponseWriter, req *http.Request) {
-  var msg string
-  if is_success {
-    msg = "opened"
-  } else {
-    msg = "not found"
-  }
-
-  fmt.Fprintf(res, "Database status is %s", msg)
+  fmt.Fprintf(res, "Database status is")
 }
