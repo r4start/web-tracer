177ac59cbac42553c3c2d0c845eb3e958f6d25a9
r4start <r4start@gmail.com>
2015-03-16 18:40:24 +0300

    Added scheme in go.

---
+++ tracer/log_scheme.go
@@ -0,0 +1,10 @@
+package tracer
+
+import "github.com/jinzhu/gorm"
+
+type LogEntry struct {
+  Id uint64
+  TerminalId uint64 `sql:"not null"`
+  Timestamp string `sql:"not null"`
+  Message string
+}
