5ad953d294a55d293695f99bc80025415478a242
r4start <r4start@gmail.com>
2015-03-19 16:58:43 +0300

    Added fault tolerance.

--- www/app/ids_fetcher.js
+++ www/app/ids_fetcher.js
@@ -4,18 +4,28 @@ var ids_fetcher = function() {
   var request = new XMLHttpRequest();
 
   request.onload = function() {
+    if (this.status != 200) {
+      return;
+    }
+
     var response_obj = JSON.parse(this.responseText);
     console.log(response_obj.ids);
 
     $('#debug_id').autocomplete({ source : response_obj.ids.map(String) });
 
-    if (id_refresher != null) {
-      clearInterval(id_refresher);
-    }
+    stop_fetching();
 
     setInterval(ids_fetcher, 10000);
   };
 
   request.open("GET", "http://" + $('#service_addr').val() + "/ids", true);
   request.send();
+};
+
+var stop_fetching = function() {
+  if (id_refresher === null) {
+    return;
+  }
+
+  clearInterval(id_refresher);
 };
