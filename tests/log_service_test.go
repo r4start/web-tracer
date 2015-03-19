22741863e4a966158dbd17183f53be80396eb153
r4start <r4start@gmail.com>
2015-03-19 19:29:54 +0300

    Working on test.

--- tests/log_service_test.go
+++ tests/log_service_test.go
@@ -1,11 +1,16 @@
 package tracercheck
 
 import (
+  "fmt"
+  "log"
   "testing"
+  "io/ioutil"
+  "net/http"
+  "net/http/httptest"
 
   "github.com/r4start/web-tracer/tracer"
 )
 
 func TestLogService(t *testing.T) {
-  
+  // See http://golang.org/pkg/net/http/httptest/
 }
