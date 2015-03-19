98d2a5d4d571af87a28bdf339cdcf038ef2e3f3a
r4start <r4start@gmail.com>
2015-03-19 19:10:14 +0300

    Move http server functionality to package files. It helps with testing.

--- server.go
+++ server.go
@@ -1,26 +1,13 @@
 package main
 
 import (
-  "os"
-  "fmt"
-  "log"
   "flag"
-  "net/http"
-
-  "github.com/gorilla/mux"
 
   "github.com/r4start/web-tracer/tracer"
 )
 
-type ServerParameters struct {
-  Host string
-  Port string
-  DbName string
-  SiteRoot string
-}
-
-func getServeAddress() ServerParameters {
-  var params ServerParameters
+func getServeAddress() tracer.ServerParameters {
+  var params tracer.ServerParameters
 
   flag.StringVar(&params.Host, "host", "localhost", "IP address for listening")
   flag.StringVar(&params.Port, "port", "80", "Port number")
@@ -32,62 +19,8 @@ func getServeAddress() ServerParameters {
   return params
 }
 
-func notFoundPage(res http.ResponseWriter, req *http.Request) {
-  res.Header().Add("Location", "http://" + req.Host + "/404.html")
-  res.WriteHeader(404)
-}
-
-func isSiteRootExists(path string) bool {
-  _, err := os.Stat(path)
-  if err == nil { return true }
-  return false
-}
-
 func main() {
   params := getServeAddress()
 
-  router := mux.NewRouter()
-  router.NotFoundHandler = http.HandlerFunc(notFoundPage)
-
-  var idsCache = tracer.NewTerminalIdsCache()
-
-  {
-    writeHandler, err := tracer.NewDbLogger(params.DbName)
-    if err != nil {
-      log.Fatal(err)
-    } else {
-      writeHandler.IdsCache = &idsCache
-      router.Handle("/terminal/{id:[0-9]+}", writeHandler)
-    }
-
-    writeHandler.PrepareDB()
-  }
-
-  {
-    idsLister, err := tracer.NewIdLister(params.DbName)
-    if err != nil {
-      log.Fatal(err)
-    } else {
-      idsLister.IdsCache = &idsCache
-      router.Handle("/ids", idsLister)
-    }
-  }
-
-  idsCache.AppendIds(tracer.LoadIdsFromDb(params.DbName))
-
-  if isSiteRootExists(params.SiteRoot) {
-    router.PathPrefix("/").Handler(http.FileServer(http.Dir(params.SiteRoot)))
-  }
- 
-  http.Handle("/", router)
-
-  bind := fmt.Sprintf("%s:%s", params.Host, params.Port)
-  
-  fmt.Printf("Listening on %s. Use database %s\n", bind, params.DbName)
-  
-  err := http.ListenAndServe(bind, nil)
-  
-  if err != nil {
-    panic(err)
-  }
+  tracer.StartServer(params)
 }
---
+++ tracer/app.go
@@ -0,0 +1,75 @@
+package tracer
+
+import (
+  "os"
+  "fmt"
+  "log"
+  "net/http"
+
+  "github.com/gorilla/mux"
+)
+
+type ServerParameters struct {
+  Host string
+  Port string
+  DbName string
+  SiteRoot string
+}
+
+func notFoundPage(res http.ResponseWriter, req *http.Request) {
+  res.Header().Add("Location", "http://" + req.Host + "/404.html")
+  res.WriteHeader(404)
+}
+
+func isSiteRootExists(path string) bool {
+  _, err := os.Stat(path)
+  if err == nil { return true }
+  return false
+}
+
+func StartServer(params ServerParameters) {
+  router := mux.NewRouter()
+  router.NotFoundHandler = http.HandlerFunc(notFoundPage)
+
+  var idsCache = NewTerminalIdsCache()
+
+  {
+    writeHandler, err := NewDbLogger(params.DbName)
+    if err != nil {
+      log.Fatal(err)
+    } else {
+      writeHandler.IdsCache = &idsCache
+      router.Handle("/terminal/{id:[0-9]+}", writeHandler)
+    }
+
+    writeHandler.PrepareDB()
+  }
+
+  {
+    idsLister, err := NewIdLister(params.DbName)
+    if err != nil {
+      log.Fatal(err)
+    } else {
+      idsLister.IdsCache = &idsCache
+      router.Handle("/ids", idsLister)
+    }
+  }
+
+  idsCache.AppendIds(LoadIdsFromDb(params.DbName))
+
+  if isSiteRootExists(params.SiteRoot) {
+    router.PathPrefix("/").Handler(http.FileServer(http.Dir(params.SiteRoot)))
+  }
+ 
+  http.Handle("/", router)
+
+  bind := fmt.Sprintf("%s:%s", params.Host, params.Port)
+  
+  fmt.Printf("Listening on %s. Use database %s\n", bind, params.DbName)
+  
+  err := http.ListenAndServe(bind, nil)
+  
+  if err != nil {
+    panic(err)
+  }
+}
