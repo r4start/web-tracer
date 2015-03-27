c4e27a10283f55eb8e30ea5bfa0e18caeebd6bdd
r4start <r4start@gmail.com>
2015-03-27 19:16:10 +0300

    Added test for `terminal ids cache`.

---
+++ tests/ids_cache_test.go
@@ -0,0 +1,19 @@
+package tracercheck
+
+import (
+  "testing"
+ 
+  _ "github.com/r4start/web-tracer/tracer"
+)
+
+func TestAppendId(t *testing.T) {
+
+}
+
+func TestAppendIds(t *testing.T) {
+  
+}
+
+func TestAppendIdsWithRepeatingValues(t *testing.T) {
+  
+}
