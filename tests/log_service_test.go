29c8612a93959d5fa6c04a9b75f188082b7aa902
r4start <r4start@gmail.com>
2015-03-27 14:03:19 +0300

    Finished with first test case.

--- tests/log_service_test.go
+++ tests/log_service_test.go
@@ -2,11 +2,13 @@ package tracercheck
 
 import (
   _ "fmt"
+  "io"
   "time"
   "bytes"
   "testing"
   "net/http"
-  _ "encoding/base64"
+  "encoding/json"
+  "encoding/base64"
   "net/http/httptest"
 
   "github.com/gorilla/mux"
@@ -31,7 +33,8 @@ func TestLogSingleMessage(t *testing.T) {
 
   recorder := httptest.NewRecorder()
   url := "/terminal/177"
-  jsonRequest := []byte(`{ "message" : "helo youg" }`)
+  messageText := "helo youg"
+  jsonRequest := []byte(`{ "message" : "` + messageText + `" }`)
 
   req, e := http.NewRequest("POST", url, bytes.NewBuffer(jsonRequest))
   if  e != nil {
@@ -40,8 +43,8 @@ func TestLogSingleMessage(t *testing.T) {
 
   go router.ServeHTTP(recorder, req)
 
-  // Timeout necessary, because the server must have some
-  // extra time for addind a new record.
+  // Timeout is necessary due the server must have some
+  // extra time for adding a new record.
   time.Sleep(5000)
 
   req, e = http.NewRequest("GET", url, nil)
@@ -53,8 +56,41 @@ func TestLogSingleMessage(t *testing.T) {
 
   router.ServeHTTP(recorder, req)
 
-  t.Log("Request and responce are not equal! ",
-        string(jsonRequest),
-        recorder.Body.String())
+  t.Log("Got responce ", recorder.Body.String())
   
+  type entryType struct {
+    Timestamp string `json:"timestamp"`
+    Message string `json:"message"`
+  }
+  type responseType struct {
+    Entries []entryType `json:"entries"`
+  }
+
+  jsonDecoder := json.NewDecoder(recorder.Body)
+
+  var logEntries responseType
+  err = jsonDecoder.Decode(&logEntries)
+  if err != nil && err != io.EOF {
+    t.Fatal(err)
+  }
+
+  if len(logEntries.Entries) != 1 {
+    t.Fatal("Log entries length doesn`t equal")
+  }
+
+  for i, v := range logEntries.Entries {
+    decodeBytes, err := base64.StdEncoding.DecodeString(v.Message)
+    if err != nil {
+      t.Fatal(err)
+    }
+
+    logEntries.Entries[i].Message = string(decodeBytes)
+  }
+
+  t.Log("Decoded json ", logEntries)
+
+  if logEntries.Entries[0].Message != messageText {
+    t.Fatal("Message text is wrong!\n Got ", logEntries.Entries[0].Message,
+            "\nShould be ", messageText)
+  }
 }
