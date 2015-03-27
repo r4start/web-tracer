5a975a0929f6dbeea57068480515988d2bfb752a
r4start <r4start@gmail.com>
2015-03-27 13:42:57 +0300

    Log service test update.

--- tests/log_service_test.go
+++ tests/log_service_test.go
@@ -2,25 +2,35 @@ package tracercheck
 
 import (
   _ "fmt"
+  "time"
   "bytes"
   "testing"
   "net/http"
+  _ "encoding/base64"
   "net/http/httptest"
+
+  "github.com/gorilla/mux"
 
   "github.com/r4start/web-tracer/tracer"
 )
 
 func TestLogSingleMessage(t *testing.T) {
-  dbName := ""
+  dbName := ":memory:"
   logger, err := tracer.NewDbLogger(dbName)
   if err != nil {
     t.Fatal(err);
   }
 
+  var idsCache = tracer.NewTerminalIdsCache()
+  logger.IdsCache = &idsCache
+
   logger.PrepareDB()
 
+  router := mux.NewRouter()
+  router.Handle("/terminal/{id:[0-9]+}", logger)
+
   recorder := httptest.NewRecorder()
-  url := "http://localhost/terminal/177"
+  url := "/terminal/177"
   jsonRequest := []byte(`{ "message" : "helo youg" }`)
 
   req, e := http.NewRequest("POST", url, bytes.NewBuffer(jsonRequest))
@@ -28,7 +38,11 @@ func TestLogSingleMessage(t *testing.T) {
     t.Fatal(e)
   }
 
-  logger.ServeHTTP(recorder, req)
+  go router.ServeHTTP(recorder, req)
+
+  // Timeout necessary, because the server must have some
+  // extra time for addind a new record.
+  time.Sleep(5000)
 
   req, e = http.NewRequest("GET", url, nil)
   if e != nil {
@@ -36,5 +50,11 @@ func TestLogSingleMessage(t *testing.T) {
   }
 
   recorder = httptest.NewRecorder()
-  logger.ServeHTTP(recorder, req)
+
+  router.ServeHTTP(recorder, req)
+
+  t.Log("Request and responce are not equal! ",
+        string(jsonRequest),
+        recorder.Body.String())
+  
 }
