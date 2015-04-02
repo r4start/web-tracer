75fe231501c9ede7cd755da88cfa9612a2350ab4
r4start <r4start@gmail.com>
2015-04-02 19:28:51 +0300

    Refactoring client code.

--- www/app/app.js
+++ www/app/app.js
@@ -1,23 +1,39 @@
+var ids_fetcher = new IdsFetcher();
+
 var submit_debug_id_by_enter = function(event) {
   if (event.which != 13) {
     return;
   }
 
-  $('#start_trace_btn').click();
+  tracer();
 };
 
-var get_ids = function(event) {
-  if (event.which != 13) {
-    return
+var get_ids = function (event) {
+  if (ids_fetcher.fetch_started) {
+    return;
   }
 
-  ids_fetcher();
+  if (event.which != 13 && $(event.target).is("#service_addr")) {
+    return;
+  }
+
+  ids_fetcher.fetch_ids();
 };
 
 var on_loaded = function() {
-  $('#start_trace_btn').on('click', tracer);
-  $('#debug_id').keyup(submit_debug_id_by_enter);
-  $('#service_addr').keyup(get_ids);
+  var dbg_id_field = $('#debug_id');
+  dbg_id_field.keyup(submit_debug_id_by_enter);
+
+  dbg_id_field.focusin(function (event) {
+    dbg_id_field.val("");
+  });
+
+  dbg_id_field.focus(get_ids);
+
+  var service_field = $('#service_addr');
+  service_field.keyup(get_ids);
+  service_field.val(location.host);
+
   $('#logs_table').tablesorter();
 };
 
--- www/app/ids_fetcher.js
+++ www/app/ids_fetcher.js
@@ -1,11 +1,16 @@
-var id_refresher = null;
+var IdsFetcher = function() {
+  this.fetch_ids = function() {
+    var request = new XMLHttpRequest();
 
-var ids_fetcher = function() {
-  var request = new XMLHttpRequest();
+    request.onload = this.ids_loaded;
 
-  request.onload = function() {
-    setTimeout(ids_fetcher, 10000);
-    
+    request.open("GET", "http://" + $('#service_addr').val() + "/ids", true);
+    request.send();
+  }
+
+  this.ids_loaded = function () {
+    setTimeout(this.fetch_ids, 10000);
+
     if (this.status != 200) {
       return;
     }
@@ -13,10 +18,6 @@ var ids_fetcher = function() {
     var response_obj = JSON.parse(this.responseText);
     console.log(response_obj.ids);
 
-    $('#debug_id').autocomplete({ source : response_obj.ids.map(String) });
-
-  };
-
-  request.open("GET", "http://" + $('#service_addr').val() + "/ids", true);
-  request.send();
+    $('#debug_id').autocomplete({source: response_obj.ids.map(String)});
+  }
 };
